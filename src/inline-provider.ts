6b9af5110166543508cb0f6fe897633b5e098d18
Koushik Kothagal <koush.s@gmail.com>
2023-01-11 21:02:48 -0800

    Refactored inline provider

---
+++ src/inline-provider.ts
@@ -0,0 +1,23 @@
+import * as vscode from 'vscode';
+
+export const registerInlineProvider = () => {
+  const provider: vscode.InlineCompletionItemProvider = {
+    provideInlineCompletionItems: async (document, position, context, token) => {
+      const lines: string[] =
+        vscode.workspace.getConfiguration('mocker').get('inlineCompletion.suggestionLines') || [];
+      if (position.line < 0) {
+        return;
+      }
+      const lineAtCursor = document.lineAt(position.line).text.trim();
+      const match = lineAtCursor.length > 3 && lines.find((str) => str.startsWith(lineAtCursor));
+      console.log(match);
+      if (match && match !== lineAtCursor) {
+        return [new vscode.InlineCompletionItem(match)];
+      } else {
+        return [];
+      }
+    },
+  };
+
+  vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, provider);
+};
